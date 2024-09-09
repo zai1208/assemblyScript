3bb2548b484c8f55e9a34a92764ca4821ee3dba0
zai1208 <zai1208@outlook.com>
2024-09-09 21:37:06 +1000

    Upload files (testing)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 import sys
 
 if __name__ == "__main__":
-	if sys.argv[1] == "-f":
-		with open(sys.argv[2], "r") as file:
-			print(file.read())
+    if sys.argv[1] == "-f":
+        with open(sys.argv[2], "r") as file:
+            if sys.argv[3] == "-o":
+                with open(sys.argv[4], "x") as output:
+                    output.write(file.read())
